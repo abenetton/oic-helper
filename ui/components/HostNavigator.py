ae601428e9de93fabc669e8a2d317f0227b7f430
Alessandro Benetton <a.benetton@reply.it>
2025-05-07 15:57:20 +0200

    Refactors host and package handling in UI
    Simplifies host and package management in the UI.
    
    - Introduces OICHost class for better host representation.
    - Modifies HostNavigator to directly use OICHost objects.
    - Improves package sorting within the HostNavigator component.
    - Removes the need for a separate HostPicker component.

--- ui/components/HostNavigator.py
+++ ui/components/HostNavigator.py
@@ -3,8 +3,7 @@ from textual.reactive import reactive
 from textual.widgets import Tree
 
 from Config import Config
-from oic_tools.OICClient import OICClient
-from oic_tools.OICPackage import OICPackage
+from oic_tools import OICHost
 
 
 class HostNavigator(Tree):
@@ -12,53 +11,39 @@ class HostNavigator(Tree):
         Binding("a", "show_all", "Show all packages")
     ]
 
-    host_id = reactive("Ajeje")
     load_all_packages = reactive(False)
+    priority_packages_sort = reactive(True)
+    hosts: reactive[dict[str, OICHost]] = reactive({})
 
-    def __init__(self, id, config: Config) -> None:
-        super().__init__(id=id, label="Ajeje")
-        self.packages = None
-        self.package_ids = None
+    def __init__(self, config: Config) -> None:
+        super().__init__("Tree root")
+        self.show_root = False
+        self.root.add_leaf("No packages loaded")
         self.config = config
+        self.hosts = config.hosts
 
-    def watch_host_id(self, host_id: str) -> None:
-        """Watch for changes to the host_id property."""
-        self.log(f"Host ID changed to: {host_id}")
-        self.border_title = f"Host Navigator - {host_id}"
-
-        # Fetch host data
-        host = self.config.hosts.get(host_id)
+    def watch_hosts(self, hosts: dict[str, OICHost]) -> None:
+        """Watch for changes to the hosts property and update the tree."""
+        self.log(f"Hosts changed to: {hosts}")
         self.root.remove_children()
-        self.root.set_label(host.label if host else "Unknown Host")
         self.root.expand()
 
-        # Add all packages from config to the tree
-        if host:
-
-            if self.load_all_packages:
-                oic_client = OICClient(host)
-                self.packages = {package.pid: package for package in oic_client.get_all_packages()}
-                package_ids = [pkg.pid for pkg in self.packages]
-
-                # Split packages into two lists, one for those with name in config and one for those not
-                packages_in_config = [pkg for pkg in package_ids if pkg in self.config.packages]
-                packages_not_in_config = [pkg for pkg in package_ids if pkg not in self.config.packages]
-
-                # Join the two lists keeping the packages in config first and sorting individual lists by name
-                self.package_ids = sorted(packages_in_config, key=lambda x: x) + sorted(packages_not_in_config,
-                                                                                        key=lambda x: x)
-            else:
-                self.packages = {package: OICPackage(package, package, 0) for package in self.config.packages}
-                self.package_ids = sorted(self.config.packages, key=lambda x: x)
-
-            for package_id in self.package_ids:
-                self.root.add(f"{self.packages.get(package_id).name} ({self.packages.get(package_id).integration_num})")
+        # Add all hosts from config to the tree
+        if hosts:
+            for host in hosts.values():
+                host_node = self.root.add(host.label)
+                # Add packages under each host
+                for package_id in host.get_sorted_package_ids(priority_packages_sort=self.priority_packages_sort):
+                    package = host.packages.get(package_id)
+                    if package:
+                        host_node.add(f"{package.name} ({package.integration_num})")
+                    else:
+                        host_node.add_leaf(f"Unknown package: {package_id}")
         else:
-            self.root.add("No packages available")
-
-        self.parent.set_loading(False)  # TODO: NOT WORKING
+            self.root.add_leaf("No hosts available")
 
     def action_show_all(self) -> None:
         """Show all packages."""
-        self.log("Showing all packages toggled")
         self.load_all_packages = not self.load_all_packages
+        self.log(f"Showing all packages toggled, new value is: {self.load_all_packages}")
+        self.root.add("Loading all packages...")
--- ui/screens/ExploreScreen.py
+++ ui/screens/ExploreScreen.py
@@ -1,18 +1,15 @@
-from textual import on
 from textual.app import ComposeResult
-from textual.binding import Binding
 from textual.screen import Screen
-from textual.widgets import Footer, Header, ContentSwitcher
+from textual.widgets import Footer, Header
 
 from Config import Config
-from ui.components import HostPicker, HostNavigator
+from ui.common.bindings import screen_common_bindings
+from ui.components import HostNavigator
 
 
 class ExploreScreen(Screen):
     """Explore screen of the OIC Helper app."""
-    BINDINGS = [
-        Binding("escape", "go_back", "Go Back")
-    ]
+    BINDINGS = screen_common_bindings
 
     def __init__(self, name=None, cid=None, classes=None):
         super().__init__(name, cid, classes)
@@ -21,23 +18,5 @@ class ExploreScreen(Screen):
 
     def compose(self) -> ComposeResult:
         yield Header()
-        with ContentSwitcher(initial="host_picker"):
-            yield HostPicker(id="host_picker", config=self.config)
-            yield HostNavigator(id="host_navigator", config=self.config)  # Valore predefinito
+        yield HostNavigator(config=self.config)
         yield Footer()
-
-    @on(HostPicker.OptionSelected)
-    def handle_option_selected(self, event: HostPicker.OptionSelected) -> None:
-        """Handle option selection."""
-        self.query_one(ContentSwitcher).set_loading(True)  # TODO: NOT WORKING
-        self.query_one(ContentSwitcher).current = "host_navigator"
-        self.query_one(HostNavigator).host_id = event.option.id
-        event.stop()
-
-    def action_go_back(self) -> None:
-        """Go back to the previous screen."""
-        if self.query_one(ContentSwitcher).current == "host_picker":
-            self.app.pop_screen()
-        else:
-            self.query_one(ContentSwitcher).current = "host_picker"
-            self.query_one(HostPicker).focus()
