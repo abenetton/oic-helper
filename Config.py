5720bf07dd4776c193c72cac3d6bca6d83d55447
Alessandro Benetton <a.benetton@reply.it>
2025-05-07 15:56:53 +0200

    Refactors config to use OICHost class
    Updates the Config class to use the OICHost class for managing host configurations. This simplifies the config structure and improves maintainability.
    Removes the old Host class.

--- Config.py
+++ Config.py
@@ -1,34 +1,25 @@
 import json
 
-class Host:
-    def __init__(self, label: str, base_url: str, token: str):
-        self.label = label
-        self.base_url = base_url
-        self.token = token
+from oic_tools.OICHost import OICHost
 
-    def __repr__(self):
-        return f"Host(label={self.label}, base_url={self.base_url}, token=****)"
 
 class Config:
-    def __init__(self, hosts: dict[str, Host] = None, packages: list[str] = None):
-        self.hosts: dict[str, Host] = hosts
-        self.packages: list[str] = packages
+    def __init__(self, hosts: dict[str, OICHost] = None, packages: list[str] = None):
+        self.hosts: dict[str, OICHost] = hosts
 
     @classmethod
     def from_dict(cls, data):
         return cls(
-            hosts=data.get("hosts", {}),
-            packages=data.get("packages", [])
+            hosts=data.get("hosts", {})
         )
 
     def load_from_file(self, file_path: str):
         with open(file_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
-        self.hosts = {key: Host(**value) for key, value in data.get("hosts", {}).items()}
-        self.packages = data.get("packages", [])
+            self.hosts = {host.get("id", ""): OICHost(**host) for host in data.get("hosts", [])}
 
     def __repr__(self):
-        return f"Config(hosts={list(self.hosts.keys())}, packages={self.packages})"
+        return f"Config(hosts={list(self.hosts.keys())})"
 
 if __name__ == "__main__":
     # Example usage
